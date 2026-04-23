dc29ef01a4f162215681331670928626bffa4d96
Francisco Navarro <franciscoalfonsonm@gmail.com>
2026-04-24 01:15:10 +0200

    created python file to test stock data downloading

---
+++ scripts/downloadYfinance.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+
+
+dat = yf.Ticker("MSFT")
+
+#One ticker
+dat = yf.Ticker("MSFT")
+dat.info
+dat.calendar
+dat.analyst_price_targets
+dat.quarterly_income_stmt
+dat.history(period='1mo')
+dat.option_chain(dat.options[0]).calls
+
+#More tickers
+tickers = yf.Tickers('MSFT AAPL GOOG')
+tickers.tickers['MSFT'].info
+#yf.download(['MSFT', 'AAPL', 'GOOG'], period='1mo')
